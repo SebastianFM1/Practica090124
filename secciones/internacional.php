a18b9389a1883dfba8e5f974f5bdf689f71da6c2
SebastianFM1 <45723245+SebastianFM1@users.noreply.github.com>
2024-01-09 22:19:21 -0500

    internacional.php

--- secciones/internacional.php
+++ secciones/internacional.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $internacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "abc",
+"autor" => "abc",
+"resumen" => "abc",
 ];
 ?>
